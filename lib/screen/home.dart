3e1d030d60078808af8210cea68afd63cc5b1d10
Kanat Aidarov <kanataidarov@yahoo.com>
2024-07-18 16:36:07 +0500

    _setIntents method added instead of static QIntent.enum

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,6 +1,9 @@
 import '../grpc/interview_automator/openai_api.pbenum.dart';
 import '../storage/memory.dart';
+import '../storage/model/qa.dart';
+import '../widget/error_page.dart';
 import '../widget/vgap.dart';
+import '../widget/waiting_page.dart';
 import 'package:flutter/material.dart';
 import 'package:interview_automator_frontend/screen/settings.dart';
 import 'package:interview_automator_frontend/service/client.dart';
@@ -32,17 +35,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   bool _isRecording = false;
   bool _recorderReady = false;
-  final Map<QIntent, bool> _subStages = {
-    QIntent.CLARIFY: false,
-    QIntent.SOLVE: true,
-    QIntent.CORRECT: false
-  };
+  final Map<QIntent, bool> _intents = {};
 
   @override
   void initState() {
     super.initState();
 
     _selectedStage = context.read<Memory>().lastStage;
+    _setIntents();
 
     if (!_recorderReady) {
       _recorder = AudioRecorder();
@@ -80,11 +80,10 @@ class _MyHomePageState extends State<MyHomePage> {
     try {
       await _recorder.start(
           const RecordConfig(
-            encoder: AudioEncoder.wav,
-            bitRate: 16000,
-            sampleRate: 11025,
-            numChannels: 1
-          ),
+              encoder: AudioEncoder.wav,
+              bitRate: 16000,
+              sampleRate: 11025,
+              numChannels: 1),
           path: filePath);
       _logger.i('Started recording - $filePath');
     } catch (e) {
@@ -143,8 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
           await SettingsProvider.instance.update(stng);
         });
 
-        final selectedIntent =
-            _subStages.keys.firstWhere((k) => _subStages[k]!);
+        final selectedIntent = _intents.keys.firstWhere((k) => _intents[k]!);
 
         _logger.d(
             'Calling ClientService.handleChatBot with parameters: ${_selectedStage.name}, ${selectedIntent.name}');
@@ -180,39 +178,55 @@ class _MyHomePageState extends State<MyHomePage> {
                                     item.name.toLowerCase()))
                               ]));
                         }).toList(),
-                        onChanged: (Stage? selectedItem) {
+                        onChanged: (Stage? selectedItem) async {
                           context.read<Memory>().setStage(selectedItem!);
                           setState(() {
                             _selectedStage = selectedItem;
                           });
+                          _intents.clear();
+                          _setIntents();
                         },
                         value: _selectedStage),
                     const VGap(height: 27),
-                    LayoutBuilder(builder: (context, limits) {
-                      return ToggleButtons(
-                        direction: Axis.horizontal,
-                        onPressed: (int selectedIndex) {
-                          QIntent intent =
-                              _subStages.keys.toList()[selectedIndex];
-                          setState(() {
-                            _subStages.updateAll(
-                                (k, v) => intent == k ? true : false);
+                    FutureBuilder<List<Qa>>(
+                      future: _setIntents(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<List<Qa>> snapshot) {
+                        Widget child;
+                        if (snapshot.hasData) {
+                          child = LayoutBuilder(builder: (context, limits) {
+                            return ToggleButtons(
+                              direction: Axis.horizontal,
+                              onPressed: (int selectedIndex) {
+                                QIntent intent =
+                                    _intents.keys.toList()[selectedIndex];
+                                setState(() {
+                                  _intents.updateAll(
+                                      (k, v) => intent == k ? true : false);
+                                });
+                              },
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8)),
+                              constraints: BoxConstraints(
+                                minHeight: 50,
+                                minWidth: limits.maxWidth / 3.1,
+                              ),
+                              isSelected: List.of(_intents.values),
+                              children: _intents.keys
+                                  .map((QIntent key) => Text(
+                                      toBeginningOfSentenceCase(
+                                          key.name.toLowerCase())))
+                                  .toList(),
+                            );
                           });
-                        },
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(8)),
-                        constraints: BoxConstraints(
-                          minHeight: 50,
-                          minWidth: limits.maxWidth / 3.1,
-                        ),
-                        isSelected: List.of(_subStages.values),
-                        children: _subStages.keys
-                            .map((QIntent key) => Text(
-                                toBeginningOfSentenceCase(
-                                    key.name.toLowerCase())))
-                            .toList(),
-                      );
-                    }),
+                        } else if (snapshot.hasError) {
+                          child = ErrorPage(snapshot: snapshot);
+                        } else {
+                          child = const WaitingPage();
+                        }
+                        return child;
+                      },
+                    ),
                     const VGap(),
                     Expanded(
                         child: RecordButton(
@@ -221,5 +235,26 @@ class _MyHomePageState extends State<MyHomePage> {
             )),
         floatingActionButton: DebugButton(context,
             isDebugEnabled: isDebugEnabled, handle: handleClient));
+  }
+
+  Future<List<Qa>> _setIntents() async {
+    if (_intents.isNotEmpty) {
+      return List.empty();
+    }
+
+    _intents.clear();
+    final qas = await (await DbHelper.instance.database).query(Qa.tableName,
+        where: 'stage = ?', whereArgs: [_selectedStage.name]);
+
+    for (var qa in qas) {
+      final intent = QIntent.values
+          .firstWhere((e) => e.name.toUpperCase() == qa['qintent']);
+      _intents[intent] = false;
+    }
+    final selectedIntent =
+        QIntent.values.firstWhere((e) => e.name.toUpperCase() == 'SOLVE');
+    _intents[selectedIntent] = true;
+
+    return List.empty();
   }
 }
